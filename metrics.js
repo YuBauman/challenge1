a3db0ff8b5a8c6718bdca410b2f35d40e3cdd096
YuBauman <78179644+YuBauman@users.noreply.github.com>
2021-05-17 20:36:33 +0200

    Delete metrics.js

--- metrics.js
+++
@@ -1,7 +0,0 @@
-var input = document.getElementById ('earth_miles');
-var convert = document.getElementById ('convert');
-var mars_miles = document.getElementById ('mars_miles');
-
-convert.onclick = function() {
-    mars_miles.value = input.value * 8;
-}
